76ddef3102505f33fbee8740414a2a7ea59a531b
Magali Cardozo <magascard1@gmail.com>
2024-01-17 19:51:34 -0300

    Agrego pagina 404 y uso React Router Dom

---
+++ src/componentes/404.jsx
@@ -0,0 +1,9 @@
+const Pagina404 = () => {
+    return(
+    <>
+    <h1>Pagina no encontrada</h1>
+    <span>La pagina solicitada no existe</span>
+    </>
+)}
+
+export default Pagina404
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import "./main.css"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Pagina404 from './componentes/404.JSX'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-    <App />
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<App/>} />
+            <Route path='*' element={<Pagina404/>}/>
+        </Routes>
+    </BrowserRouter>
   
 )
