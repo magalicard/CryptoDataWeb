0c46b7ccdd41d053518aa8f9f9c8f51757fae239
Magali Cardozo <magascard1@gmail.com>
2024-01-20 12:00:57 -0300

    cambio app por cuadricula y uso rutas anidadas

---
+++ src/componentes/Cuadricula.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+import Cripto from "./cripto"
+import "./Cuadricula.css"
+import logosimple from "/Users/Magali/proyecto-del-curso/public/logosimple.png"
+
+
+
+function Cuadricula() {
+
+  const API_URL = import.meta.env.VITE_API_URL
+  const [criptos, setCriptos] = useState()
+
+  useEffect(() => {
+    axios.get(`${API_URL}`)
+      .then((data) => {
+        //console.log(data) //nos devuelve un objeto 
+        setCriptos(data.data.data);
+      })
+      .catch(() => {
+        console.error("Algo salió mal");
+      });
+  },[])
+
+  if(!criptos) return <span>Cargando...</span>
+
+  return(
+   <div className="main-container">
+    <div className="logo">
+        <img src={logosimple} alt="Logo de la pagina" />
+    </div>
+    <h1>Lista de criptomonedas</h1>
+    <div className="cripto-container">
+      {criptos.map(({id, name, priceUsd, symbol, changePercent24Hr}) => (
+        <Cripto 
+        key={id}
+        name={name}
+        priceUsd={priceUsd}
+        symbol={symbol} 
+        changePercent24Hr={changePercent24Hr}
+        />
+        
+      ))}
+    </div>
+
+   </div>
+  )
+}
+
+export default Cuadricula
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './componentes/App.jsx'
+import App from './componentes/App'
 import "./main.css"
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Pagina404 from './componentes/404.JSX'
 import Saludo from './componentes/Saludo.jsx'
 import Menu from './menu/Menu.jsx'
+import Cuadricula from './componentes/Cuadricula.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <>
-    
     <BrowserRouter>
-       <Menu/> 
         <Routes>
-            <Route path='/' element={<App/>} />
-            <Route path='/saludo' element={<Saludo/>}/>
+            <Route path='/' element={<App/>}>
+                <Route index element={<Cuadricula/>} />
+                <Route path='saludo' element={<Saludo/>}/>
+            </Route>
             <Route path='*' element={<Pagina404/>}/>
         </Routes>
     </BrowserRouter>
-    </>
-  
 )
