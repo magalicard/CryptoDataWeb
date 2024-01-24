d8603564a7192a5aceee2676e64a18d0ba89caa3
Magali Cardozo <magascard1@gmail.com>
2024-01-23 21:23:31 -0300

    Tabla en HTML terminada (tiene error)

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom"
 import Menu from "../menu/Menu"
-
+import "./App.css"
 const App = () => {
     return (
         <>
--- src/componentes/cripto/CriptoPage.jsx
+++ src/componentes/cripto/CriptoPage.jsx
@@ -1,6 +1,8 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { parseFloatNumber } from "../../helpers/numbers"
+import "./CriptoPage.css"
 
 const CriptoPage = () => {
     
@@ -15,46 +17,71 @@ const CriptoPage = () => {
         .then(data =>
             setCripto(data.data.data) )
         .catch( e => console.error(e))
-    }, [])
 
-    useEffect(() => {
         axios.get(`${API_URL}/${params.id}/history?interval=d1`)
         .then(data =>
             setHistory(data.data.data) )
         .catch( e => console.error(e))
-    }, [])
+    }, [history])
+
+   
 
     return (
-        <>
-        <h1>Soy una criptomoneda {params.id} </h1>
-       <div className="info">
-        <ul>
-          <li><span className="label">Nombre: </span>{cripto.name}</li>
-          <li><span className="label">Simbolo: </span>{cripto.symbol}</li>
-        </ul>
-
-       </div>
-       <h2>HISTORIAL: </h2>
-       <table>
-        <thead>
-            <tr>
-                <th>Fecha</th>
-                <th>Precio</th>
-            </tr>
-        </thead>
-        <tbody>
-            {
-                history.map(({date, priceUsd, time}) => (
-                    <tr key={time}>
-                         <td>{date}</td> {/* date.js para darle forma */}
-                        <td>{priceUsd}</td>
-                    </tr>
-                ))
-            }
-        </tbody>
-       </table>
-
-        </>
+        <div className="cripto-page-container">
+            <div className="info">
+                <div className="main-info">
+                    <span>Ranking: {cripto.rank}</span>
+                    <h1>{cripto.name}</h1>
+                    <span className="symbol">{cripto.symbol}</span>
+                </div>
+                <div className="details">
+                    <ul>
+                        <li className="detail">
+                           <span className="label">Precio: </span>
+                           <span>{parseFloatNumber(cripto.priceUsd, 3)}</span>
+                        </li>
+                        <li className="detail">
+                            <span className="label">MaxSupply: </span>
+                            <span>{parseFloatNumber(cripto.maxSupply, 3)}</span>
+                        </li>
+                        <li className="detail">
+                            <span className="label">Market cap (USD): </span>
+                            <span>{parseFloatNumber(cripto.marketCapUsd, 3)}</span>
+                        </li>
+                        <li className="detail">
+                            <span className="label">Volumen (USD - 24 Hrs.): </span>
+                            <span>{parseFloatNumber(cripto.volumeUsd24Hr, 3)}</span>
+                        </li>
+                        <li className="detail">
+                            <span className="label">Variacion (24 Hrs.): </span>
+                            <span>{parseFloatNumber(cripto.changePercent24Hr, 3)}</span>
+                        </li>
+                        <li className="detail">
+                            <span className="label">Vwap 24 Hrs.: </span>
+                            <span>{parseFloatNumber(cripto.vwap24Hr, 3)}</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="history">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Fecha</th>
+                            <th>Precio</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {history.reverse().map(({date, priceUsd, time}) => (
+                            <tr key={time}>
+                                <td className="label">{new Date(date).toDateString()}</td>
+                                <td className="price">{parseFloatNumber(priceUsd, 3)}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
     )
 }
 
